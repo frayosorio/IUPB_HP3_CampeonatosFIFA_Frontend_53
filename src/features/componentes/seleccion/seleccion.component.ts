ce80c765e0f935b6dab745fb6e5e9fd83ae8f2ae
fray osorio <frayosorio@gmail.com>
2025-04-08 09:44:20 -0500

    Se agregan elementos de busqueda en elcomponente de SELECCIONES

--- src/features/componentes/seleccion/seleccion.component.ts
+++ src/features/componentes/seleccion/seleccion.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { ReferenciasMaterialModule } from '../../../shared/modulos/referencias-material.module';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-seleccion',
   imports: [
     ReferenciasMaterialModule,
+    FormsModule,
+    NgFor
   ],
   templateUrl: './seleccion.component.html',
   styleUrl: './seleccion.component.css'
 })
 export class SeleccionComponent {
+
+  public opcionBusqueda: number = -1;
+  public textoBusqueda: string = "";
+  public opcionesBusqueda: string[] = ["Nombre", "Entidad regente"];
+
+
+  public buscar(){
+
+  }
 
 }
