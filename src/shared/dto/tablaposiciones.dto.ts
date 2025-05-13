c0e06023115ad1767c58a063e4f446611c002b8c
fray osorio <frayosorio@gmail.com>
2025-05-13 09:42:44 -0500

    Se agrega DTO para tabla de posiciones y servicio para consumir controlador GRUPOS de la API

---
+++ src/shared/dto/tablaposiciones.dto.ts
@@ -0,0 +1,10 @@
+export interface TablaPosicionesDTO {
+    Pais: string,
+    PJ: number,
+    PG: number,
+    PE: number,
+    PP: number,
+    GF: number,
+    GC: number,
+    Puntos: number,
+}
