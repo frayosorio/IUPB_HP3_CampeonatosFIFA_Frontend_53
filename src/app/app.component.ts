2d89ae34faff1a324110a7c4990bfb46f67e960c
fray osorio <frayosorio@gmail.com>
2025-05-22 09:28:33 -0500

    Se agrega menu lateral de opciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { ReferenciasMaterialModule } from '../shared/modules/referencias-material.module';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,
-    ReferenciasMaterialModule
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    ReferenciasMaterialModule,
+    NgFor
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CampeonatosFIFA';
+
+  public opciones = [
+    { titulo: "Selecciones", url: "selecciones", icono: "iconos/Seleccion.png" },
+    { titulo: "Campeonatos", url: "campeonatos", icono: "iconos/Campeonato.png" },
+    { titulo: "Grupos", url: "grupos", icono: "iconos/Grupo.png" },
+    { titulo: "Encuentros", url: "", icono: "iconos/Encuentro.png" },
+    { titulo: "Ciudades", url: "", icono: "iconos/Ciudad.png" },
+    { titulo: "Estadios", url: "", icono: "iconos/Estadio.png" },
+    { titulo: "Fases", url: "", icono: "iconos/Fase.png" }
+  ];
+
 }
